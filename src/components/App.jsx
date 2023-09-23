236a4a77bbd1531e6131f3348a35653f93c8cea8
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-23 10:39:04 +0200

    added in marking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  changeOfState = clickBtnGood => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <h2 className="title-feedback">Please leave feedback</h2>
+        <button onClick={event.target.value} type="button" className="btn-good">
+          Good
+        </button>
+        <button
+          onClick={evt => {
+            console.log('Cliked in btn-neutral!', evt); // працює
+          }}
+          type="button"
+          className="btn-neutral"
+        >
+          Neutral
+        </button>
+        <button
+          onClick={evt => {
+            console.log('Cliked in btn-bed!', evt); // працює
+          }}
+          type="button"
+          className="btn-bad"
+        >
+          Bad
+        </button>
+        <h2 className="title-statistics">Statistics</h2>
+        <p className="good">
+          Good:
+          <span className="number-good">3</span>
+        </p>
+        <p className="neutral">
+          Neutral:
+          <span className="number-neutral">2</span>
+        </p>
+        <p className="bad">
+          Bad:
+          <span className="number-bad">2</span>
+        </p>
+      </div>
+    );
+  }
+}
